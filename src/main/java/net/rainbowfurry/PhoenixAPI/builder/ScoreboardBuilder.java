aa493c2c2865d089b77c67fb430fd275430ca88d
Jasmin Hoffmannn <jasmin98.hoffmann@gmail.com>
2024-12-27 17:33:46 +0100

    Modified comments

--- src/main/java/net/rainbowfurry/PhoenixAPI/builder/ScoreboardBuilder.java
+++ src/main/java/net/rainbowfurry/PhoenixAPI/builder/ScoreboardBuilder.java
@@ -11,25 +11,43 @@ public class ScoreboardBuilder {
     private Objective objective;
 
     //ToDo: Name mit speichern? falls ja aber nicht multiscore möglich außer seperater aufruf
+
+    private int placeholderIndex = 1;
+
+    /**
+     * Create a new ScoreBoard
+     * @param name Score Name
+     * @param displayName Score DisplayName
+     * @return base class
+     */
     public ScoreboardBuilder createScoreBoard(String name, String displayName){
         assert manager != null;
         scoreboard = manager.getNewScoreboard();
-        // Create an objective
         objective = scoreboard.registerNewObjective(
-                name, // Internal name
-                Criteria.DUMMY,     // Type
-                displayName // Display name
+                name,
+                Criteria.DUMMY,
+                displayName
         );
         objective.setDisplaySlot(DisplaySlot.SIDEBAR);
         return this;
     }
 
+    /**
+     * Add new Line
+     * @param content Content
+     * @param index Index
+     * @return base Class
+     */
     public ScoreboardBuilder addObject(String content, int index){
         objective.getScore(content).setScore(index);
         return this;
     }
 
-    int placeholderIndex = 1;
+    /**
+     * Add empty Line as PlaceHolder
+     * @param index Line Index
+     * @return base Class
+     */
     public ScoreboardBuilder addPlaceholder(int index){
         String placeholder = "";
         for(int i = 0; i < placeholderIndex; i++)
@@ -39,10 +57,23 @@ public class ScoreboardBuilder {
         return this;
     }
 
+    /**
+     * Build the ScoreBoard
+     * @return ScoreBoard
+     */
     public Scoreboard build(){
         return scoreboard;
     }
 
+    /**
+     * Update Score without flicker
+     * @param player Player
+     * @param title Score Title
+     * @param old Old Content
+     * @param content New Content
+     * @param index Index
+     * @return base Class
+     */
     public ScoreboardBuilder updateObject(Player player, String title, String old, String content, int index){
         Scoreboard playerScore = player.getScoreboard();
         Objective objective = playerScore.getObjective(title);
