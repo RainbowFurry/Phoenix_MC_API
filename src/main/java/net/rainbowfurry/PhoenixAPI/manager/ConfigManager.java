1cea65492c86f2a20ed9e100003f46018677e380
Jasmin Hoffmannn <jasmin98.hoffmann@gmail.com>
2024-12-27 17:33:54 +0100

    Modified comments

--- src/main/java/net/rainbowfurry/PhoenixAPI/manager/ConfigManager.java
+++ src/main/java/net/rainbowfurry/PhoenixAPI/manager/ConfigManager.java
@@ -1,6 +1,5 @@
 package net.rainbowfurry.PhoenixAPI.manager;
 
-import net.rainbowfurry.PhoenixAPI.PhoenixAPI;
 import org.apache.commons.io.FileUtils;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.configuration.file.YamlConfiguration;
@@ -17,12 +16,20 @@ public class ConfigManager {
     private YamlConfiguration config;//FileConifguration
     private JavaPlugin plugin;
 
+    /**
+     * Init ConfigManager and load Config File
+     * @param fileName Config File path and name with ending
+     * @param plugin Your Plugin Instance
+     */
     public ConfigManager(String fileName, JavaPlugin plugin) {
         this.fileName = fileName;
         this.plugin = plugin;
         this.configFile = new File(plugin.getDataFolder(), fileName);
     }
 
+    /**
+     * Create the Config File if not exist
+     */
     public void createConfig() {
         if (!configFile.exists()) {
             try {
@@ -35,6 +42,13 @@ public class ConfigManager {
         config = YamlConfiguration.loadConfiguration(configFile);
     }
 
+    /**
+     * Create the default ConfigFile from your "resources" Folder
+     * The path on your Server and in your resources Filder need to be
+     * identical to work.
+     * @param plugin Your Plugin Instance
+     * @param fileName File path and name with ending
+     */
     public void createDefaultConfig(JavaPlugin plugin, String fileName) {
         if (!configFile.exists()) {
             try {
@@ -53,6 +67,10 @@ public class ConfigManager {
         config = YamlConfiguration.loadConfiguration(configFile);
     }
 
+    /**
+     * Get the Current ConfigFile
+     * @return FileConfiguration
+     */
     public FileConfiguration getConfig() {
         if (config == null) {
             config = YamlConfiguration.loadConfiguration(configFile);
@@ -60,6 +78,9 @@ public class ConfigManager {
         return config;
     }
 
+    /**
+     * Save the Config File
+     */
     public void saveConfig() {
         try {
             if (config != null) {
@@ -70,6 +91,9 @@ public class ConfigManager {
         }
     }
 
+    /**
+     * Reload the Config File
+     */
     public void reloadConfig() {
         config = YamlConfiguration.loadConfiguration(configFile);
     }
--- src/main/java/net/rainbowfurry/PhoenixAPI/manager/FileManager.java
+++ src/main/java/net/rainbowfurry/PhoenixAPI/manager/FileManager.java
@@ -5,28 +5,72 @@ import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
 public class FileManager {
+
+    /*
+    * This File is still in Progress and not tested or completed!
+    * */
 
     private File file;
     private FileConfiguration config;
     private YamlConfiguration yamlConfiguration;
+    private List<FileConfiguration> configs = new ArrayList<>();
 
-    public void loadAllFiles(String path){
-
+    @Deprecated
+    public List<FileConfiguration> loadAllConfigs(File directory, boolean subFolder){
+        if (directory.exists() && directory.isDirectory()) {
+            File[] files = directory.listFiles();
+            if (files != null) {
+                for (File file : files) {
+                    if (file.isFile()) {
+                        if(file.length() > 0)
+                            configs.add(YamlConfiguration.loadConfiguration(file));
+                    } else if (file.isDirectory()) {
+                        if(subFolder)
+                            loadAllConfigs(file, true);
+                    }
+                }
+            }
+        }
+        return configs;
     }
 
-    private void loadFile(String file){
-
+    @Deprecated
+    public List<FileConfiguration> loadAllConfigs(JavaPlugin plugin, boolean subFolders){
+        return loadAllConfigs(plugin.getDataFolder(), subFolders);
     }
 
-    private void loadFile(String file, boolean validate){
-
+    @Deprecated
+    public YamlConfiguration loadConfig(String fileName, boolean validate){
+        this.file = new File(fileName);
+        if(file.exists() && file.isFile()){
+            if(validate)
+                if(isValidFile(this.file))
+                    return YamlConfiguration.loadConfiguration(file);
+        }
+        return null;
     }
 
-    private void createDefaults(JavaPlugin plugin, String file){
+    @Deprecated
+    public YamlConfiguration loadConfig(JavaPlugin plugin, String file, boolean validate){
+        return loadConfig(plugin.getDataFolder() + file, validate);
+    }
 
-        InputStream inputStream =  plugin.getClass().getResourceAsStream(file);
+    @Deprecated
+    /**
+     * Creates the Default Config File of your selected File
+     * Your Defaults Config needs to be existing in the "resources" Folder
+     * At the same Location in your "plugins" Folder the new Config File will be created.
+     * @param plugin Your Plugin Instance
+     * @param file Your File Path + File Name + File Ending
+     */
+    public void createDefaults(JavaPlugin plugin, String file){
+        //ToDo check if file exists before
+        InputStream inputStream = plugin.getClass().getResourceAsStream(file);
         if (inputStream != null) {
             yamlConfiguration = YamlConfiguration.loadConfiguration(new InputStreamReader(inputStream));
             File outputFile = new File(plugin.getDataFolder(), file);
@@ -38,16 +82,15 @@ public class FileManager {
             } catch (Exception e) {
                 e.printStackTrace();
             }
-
-//            try (BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream))) {
-//                String line;
-//                while ((line = reader.readLine()) != null) {
-//                    System.out.println(line);
-//                }
-//            } catch (IOException e) {
-//                e.printStackTrace();
-//            }
         }
+    }
+
+    @Deprecated
+    public boolean contains(FileConfiguration config, String key){
+        Map<String, Object> _object = config.getConfigurationSection(key).getValues(false);
+        if(_object.keySet() != null)
+            return true;
+        return false;
     }
 
     /**
@@ -55,28 +98,35 @@ public class FileManager {
      * @param file YML File
      * @return Is Valid
      */
-    private boolean isValidFile(File file){
+    @Deprecated
+    public boolean isValidFile(File file){
+        config = YamlConfiguration.loadConfiguration(file);
+        return isValidConfig(config);
+    }
 
+    @Deprecated
+    public boolean isValidConfig(FileConfiguration config){
+        this.config = config;
         try {
-        // Check if File exists and is Valid YML
-        if (file == null || !file.exists() || !file.isFile()) {
-            System.out.println("YAML file is missing or invalid.");
-            return false;
-        }
+            // Check if File exists and is Valid YML
+            if (!file.exists() && !file.isFile()) {
+                System.out.println("YAML file is missing or invalid.");
+                return false;
+            }
 
-        // Check if the file is empty or has no meaningful content
-        if (config.getKeys(false).isEmpty()) {
-            System.out.println("YAML file is empty or contains no top-level keys.");
-            return false;
-        }
+            // Check if the file is empty or has no meaningful content
+            if (config.getKeys(false).isEmpty() || file.length() == 0) {
+                System.out.println("YAML file is empty or contains no top-level keys.");
+                return false;
+            }
 
-        // Add specific checks for required content
+            // Add specific checks for required content
 //        if (!config.contains("Buildings")) {
 //            System.out.println("YAML file is missing required 'Buildings' section.");
 //            return false;
 //        }
 
-        return true;
+            return true;
         } catch (Exception e) {
 //            System.out.println("Error while loading YAML file: " + e.getMessage());
 //            e.printStackTrace();
