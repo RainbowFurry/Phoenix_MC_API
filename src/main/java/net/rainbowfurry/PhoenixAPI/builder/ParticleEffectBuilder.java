13b40c53313b6edf50b61a39529b0aabc4820ec2
Jasmin Hoffmannn <jasmin98.hoffmann@gmail.com>
2024-12-27 17:31:49 +0100

    Added ParticleEffectBuilder.java

---
+++ src/main/java/net/rainbowfurry/PhoenixAPI/builder/ParticleEffectBuilder.java
@@ -0,0 +1,13 @@
+package net.rainbowfurry.PhoenixAPI.builder;
+
+public class ParticleEffectBuilder {
+
+    /*
+    * new ParticleEffectBuilder(Particle.FLAME)
+    .setAmount(10)
+    .setOffset(0.5, 0.5, 0.5)
+    .setLocation(player.getLocation())
+    .play();
+    * */
+
+}
