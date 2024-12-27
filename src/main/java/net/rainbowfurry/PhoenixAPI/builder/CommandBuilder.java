99abfd194391e703b1f1b28b51beca4c3f2dad0a
Jasmin Hoffmannn <jasmin98.hoffmann@gmail.com>
2024-12-27 17:32:16 +0100

    Added CommandBuilder.java (Temp)

---
+++ src/main/java/net/rainbowfurry/PhoenixAPI/builder/CommandBuilder.java
@@ -0,0 +1,16 @@
+package net.rainbowfurry.PhoenixAPI.builder;
+
+public class CommandBuilder {
+
+    /*
+    * new CommandBuilder("hello")
+    .setDescription("Sends a greeting")
+    .setPermission("myplugin.hello")
+    .setExecutor((sender, args) -> {
+        sender.sendMessage("Hello, world!");
+        return true;
+    })
+    .register();
+    * */
+
+}
