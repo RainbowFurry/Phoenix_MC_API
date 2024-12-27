d15a087abcf4a6fc5d82f3e052fba65c42fca01c
Jasmin Hoffmannn <jasmin98.hoffmann@gmail.com>
2024-12-27 17:32:04 +0100

    Added RecipeBuilder.java

---
+++ src/main/java/net/rainbowfurry/PhoenixAPI/builder/RecipeBuilder.java
@@ -0,0 +1,13 @@
+package net.rainbowfurry.PhoenixAPI.builder;
+
+public class RecipeBuilder {
+
+    /*
+    * new RecipeBuilder("custom_sword", new ItemBuilder(Material.DIAMOND_SWORD).setName("§cCustom Sword").build())
+    .shape(" D ", " D ", " S ")
+    .setIngredient('D', Material.DIAMOND)
+    .setIngredient('S', Material.STICK)
+    .build();
+    * */
+
+}
