55b4498441d564256eed550dac150adee8d0d46d
Jasmin Hoffmannn <jasmin98.hoffmann@gmail.com>
2024-12-29 15:21:54 +0100

    Added LocationBuilder.java

--- src/main/java/net/rainbowfurry/PhoenixAPI/builder/LocationBuilder.java
+++ src/main/java/net/rainbowfurry/PhoenixAPI/builder/LocationBuilder.java
@@ -15,53 +15,103 @@ public class LocationBuilder {
     private float yaw = 0.5f;
     private float pitch = 0.5f;
 
+    /**
+     * Init locationBuilder blank
+     * (if values are missing Exeption could appear)
+     */
     public LocationBuilder(){
 
     }
 
+    /**
+     * Init LocationBuilder with the Player Location
+     * @param player Player
+     */
     public LocationBuilder(Player player){
         this.location = player.getLocation();
     }
 
+    /**
+     * Init LocationBuilder with Locationt to modify
+     * @param location Location
+     */
     public LocationBuilder(Location location){
         this.location = location;
     }
 
+    /**
+     * Init LocationBuilder with World
+     * @param world World
+     */
     public LocationBuilder(World world){
         this.world = world;
     }
 
+    /**
+     * Set the World for the Location
+     * @param world World
+     * @return base Class
+     */
     public LocationBuilder setWorld(World world){
         this.world = world;
         return this;
     }
 
+    /**
+     * Set the X Coordinate for the Location
+     * @param x X Coordinate
+     * @return base Class
+     */
     public LocationBuilder setX(double x){
         this.x = x;
         return this;
     }
 
+    /**
+     * Set the Y Coordinate for the Location
+     * @param y Y Coordinate
+     * @return  base Class
+     */
     public LocationBuilder setY(double y){
         this.y = y;
         return this;
     }
 
+    /**
+     * Set the Z Coordinate for the Location
+     * @param z Z Coordinate
+     * @return base Class
+     */
     public LocationBuilder setZ(double z){
         this.z = z;
         return this;
     }
 
+    /**
+     * Set the Yaw Coordinate for the Location
+     * @param yaw Yaw
+     * @return base Class
+     */
     public LocationBuilder setYaw(float yaw){
         this.yaw = yaw;
         return this;
     }
 
+    /**
+     * Set the Pitch Coordinate for the Location
+     * @param pitch Pitch
+     * @return base Class
+     */
     public LocationBuilder setPitch(float pitch){
         this.pitch = pitch;
         return this;
     }
 
-    public Location modifyBuild(){
+    /**
+     * Get modified Location
+     * @return Location
+     */
+    public Location modify(){
         location.setWorld(world);
         location.setX(x);
         location.setY(y);
@@ -71,6 +121,10 @@ public class LocationBuilder {
         return location;
     }
 
+    /**
+     * Get Location
+     * @return Location
+     */
     public Location build(){
         this.location = new Location(world, x, y, z, yaw, pitch);
         return location;
