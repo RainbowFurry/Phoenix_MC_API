9f9b8a505c7014b68ce6c5527aa1e2b9ae8559fb
Jasmin Hoffmannn <jasmin98.hoffmann@gmail.com>
2025-01-05 13:03:27 +0100

    Added RecipeBuilder.java

--- src/main/java/net/rainbowfurry/PhoenixAPI/builder/RecipeBuilder.java
+++ src/main/java/net/rainbowfurry/PhoenixAPI/builder/RecipeBuilder.java
@@ -1,6 +1,25 @@
 package net.rainbowfurry.PhoenixAPI.builder;
 
+import org.bukkit.Material;
+import org.bukkit.inventory.ItemStack;
+
 public class RecipeBuilder {
+
+    public RecipeBuilder(String name, ItemStack itemStack){
+
+    }
+
+    public void shape(String row1, String row2, String row3){
+
+    }
+
+    public void setIngredient(char ingredient, Material material){
+
+    }
+
+    public void build(){
+
+    }
 
     /*
     * new RecipeBuilder("custom_sword", new ItemBuilder(Material.DIAMOND_SWORD).setName("§cCustom Sword").build())
